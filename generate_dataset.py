1aed9339436aca02c960c4934f0fa950f256bedc
Chris Bethune <cbethune@uncharted.software>
2020-09-30 09:18:53 -0400

    support subsampling

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,9 +1,11 @@
 import argparse
+from typing import List
 import zipfile
 import os
 import shutil
 import tqdm
 import pandas as pd
+import random
 
 def get_file_id(geohash, date):
     year, month, day = date.split('-')
@@ -14,6 +16,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--features_csv', type=str)
     parser.add_argument('--data_location', type=str)
+    parser.add_argument('--output_location', type=str)
+    parser.add_argument('--negative_sample', default=1.0, type=str)
 
     return parser.parse_args()
 
@@ -29,8 +33,8 @@ for pos_row in positive_features.iterrows():
     id = get_file_id(pos_row[1]['geohash'], pos_row[1]['date'])
     pos_feature_ids.add(id)
 
-pos_path = os.path.join(args.data_location, "hoppers")
-neg_path = os.path.join(args.data_location, "no_hoppers")
+pos_path = os.path.join(args.output_location, "hoppers")
+neg_path = os.path.join(args.output_location, "no_hoppers")
 
 # make directories for positive and negative labels
 os.makedirs(pos_path, exist_ok=True)
@@ -39,11 +43,26 @@ os.makedirs(neg_path, exist_ok=True)
 # loop over file and move to the correct directories
 print('Moving files to label directories')
 entries = os.listdir(args.data_location)
+neg_files = {}
 for entry in tqdm.tqdm(entries):
     if os.path.isfile(os.path.join(args.data_location, entry)):
         geohash, date, _ = entry.split('_')
-        unique_id = f'{geohash}_{date}'
-        if unique_id in pos_feature_ids:
-            shutil.move(os.path.join(args.data_location, entry), pos_path)
+        tile_id = f'{geohash}_{date}'
+        if tile_id in pos_feature_ids:
+            # copy to pos directory right away
+            shutil.copy(os.path.join(args.data_location, entry), pos_path)
         else:
-            shutil.move(os.path.join(args.data_location, entry), neg_path)
+            # store the negative examples and process them after the fact since
+            # we want randomize and shuffle
+            if tile_id not in neg_files:
+                neg_files[tile_id] = []
+            neg_files[tile_id].append(os.path.join(args.data_location, entry))
+
+# copy over a random sample of negative files
+neg_files_list = list(neg_files.items())
+random.shuffle(neg_files_list)
+num_neg_files = int(len(neg_files_list) * float(args.negative_sample))
+for idx in tqdm.trange(num_neg_files):
+    for path in neg_files_list[idx][1]:
+        shutil.copy(path, neg_path)
+
--- unzip_downloaded.py
+++ unzip_downloaded.py
@@ -26,12 +26,21 @@ for entry in tqdm.tqdm(entries):
         temp_entries = os.listdir(temp_path)
     for temp_entry in temp_entries:
         # generate a new sentinel-like file name for the unzipped entry
-        _, band, _ = temp_entry.split('.')
 
+        # get the band into sentinel format, ignore the quality file
+        # 'B1' -> 'B01'
+        _, band, _ = temp_entry.split('.')
+        if band == 'QA60':
+            continue
+        if len(band) < 3:
+            band = f'{band[0]}0{band[1]}'
+
+        # get the date into the sentinel format
+        # 20201112 -> 20201112T000000
         year, month, day = date.split('-')
         datestr = f'{year}{month}{day}T000000'
 
-        output_file = os.path.join(args.output_location,  '{id}_{datestr}_{band}.tif')
+        output_file = os.path.join(args.output_location,  f'{id}_{datestr}_{band}.tif')
         # copy the file to the output dir using its new name
         shutil.copyfile(os.path.join(temp_path,  temp_entry), output_file)
     shutil.rmtree(temp_path)
